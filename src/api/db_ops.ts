de16bf055fa337ba56d2a9df0eb7fd3f820b1c59
ttyago <ttyago.dev@gmail.com>
2022-01-26 12:28:53 +0100

    Implement onSend function & SUPABASE_DB Schema

---
+++ src/api/db_ops.ts
@@ -0,0 +1,41 @@
+// DB operations method
+import { createClient } from "@supabase/supabase-js"
+import YAML from "yaml"
+import * as fs from "fs"
+import axios from "axios"
+
+const config = YAML.parse(fs.readFileSync('src/config.yml', 'utf-8'))
+
+const admin_client = createClient(config.SUPABASE_DB.URL, config.SUPABASE_DB.SEC_KEY)
+const public_client = createClient(config.SUPABASE_DB.URL, config.SUPABASE_DB.PUB_KEY)
+
+
+//curl "https://api.twitter.com/2/tweets?expansions=author_id" -H "Authorization: Bearer $BEARER_TOKEN"
+async function getAuthID(tweet_id: string){
+    const URL = `https://api.twitter.com/2/tweets/${tweet_id}?expansions=author_id`
+
+    const api_call = await axios.get(URL, {
+        headers : {
+            'Authorization': `Bearer ${config.TWITTER_API.BEARER_TOKEN}`
+        }
+    })
+
+    return api_call.data.data.author_id
+}
+
+async function onSend(address_from: string, value: number, tweet_id: string){
+    // Register address[msg.sender], value, chain, 
+    const _authID = await getAuthID(tweet_id)
+
+    let {data, error} = await admin_client.from('tip_to_tweet').insert([{
+        address_from: address_from, value: value, tweet_id: tweet_id, auth_id: _authID
+    }])
+
+    if(data) { return } 
+    console.error(error)
+}
+
+
+async function onClaim(){
+
+}
--- src/scripts/deploy-tip.ts
+++ src/scripts/deploy-tip.ts
@@ -1,19 +1,16 @@
-import { ethers, providers, Signer } from "ethers";
+import { Contract, ethers, providers, Signer } from "ethers";
 import { abi, bytecode } from "../artifacts/src/contracts/tip.sol/Tipping.json"
-// import YAML from "yaml"
-// import * as fs from "fs"
 
-// // Currently move this const in external file and export not work:
-// // ERROR: no file to open
-// const local_config = YAML.parse(fs.readFileSync('../config.local.yml','utf-8'))
-
-// export const provider = new ethers.providers.JsonRpcProvider(local_config.HARDHAT.provider)
-
-export async function deploy_tip (_signerPrivate_key: string,_provider: ethers.providers.JsonRpcProvider ) {
+// Maybe we can choose to abstract this function and add a parameters to choose the contract (_contractInstance?)
+export async function deploy_tip (_signerPrivate_key: string,_provider: ethers.providers.JsonRpcProvider, _contractInstace?: Contract ) {
+    
     // Signer setup (owner of the smart contract)
     const signer = new ethers.Wallet(_signerPrivate_key, _provider)
 
     // Generate contract
+    if(_contractInstace){
+        // Let's deploy the _contractInstance
+    }
     const tip_factory = new ethers.ContractFactory(abi, bytecode, signer)
     const tip_contract = await tip_factory.deploy()
 
